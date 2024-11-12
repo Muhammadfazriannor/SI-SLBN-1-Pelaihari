5a7d3e80a2dbcb0f0c8dec1781f35be9f75fcd55
MRD_EXE <68524961+MRD127@users.noreply.github.com>
2024-11-12 14:07:37 +0800

    update dasboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', function () {
+    return view('dashboard');
+});
 
 //route resource for products
 Route::resource('/pengumumen', \App\Http\Controllers\PengumumanController::class);
