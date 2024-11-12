a12036447e64559f7f3a0b632d67cd8d585aa2c6
MRD_EXE <68524961+MRD127@users.noreply.github.com>
2024-11-12 13:47:07 +0800

    hilangkan judul

--- resources/views/pengumumen/index.blade.php
+++ resources/views/pengumumen/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data Pengumuman - SantriKoding.com</title>
+    <title>Data Pengumuman</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body style="background: lightgray">
@@ -12,11 +12,6 @@
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
-                <div>
-                    <h3 class="text-center my-4">Tutorial Laravel 11 untuk Pemula</h3>
-                    <h5 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h5>
-                    <hr>
-                </div>
                 <div class="card border-0 shadow-sm rounded">
                     <div class="card-body">
                         <a href="{{ route('pengumumen.create') }}" class="btn btn-md btn-success mb-3">ADD PENGUMUMAN</a>
