dc37d087c9ab4cec03d32905a11cfbb7d4c8246c
MRD_EXE <68524961+MRD127@users.noreply.github.com>
2024-11-12 14:38:36 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PendaftarController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
 
 //route resource for products
