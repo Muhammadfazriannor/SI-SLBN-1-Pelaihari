552b79f7fe2c038b62d4596d89a70bf7e0413a76
ricky-qwerty <ricky2633oke@gmail.com>
2024-11-12 14:27:29 +0800

    push

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PendaftarController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -8,3 +9,4 @@ Route::get('/', function () {
 
 //route resource for products
 Route::resource('/pengumumen', \App\Http\Controllers\PengumumanController::class);
+Route::resource('/pendaftars', \App\Http\Controllers\PendaftarController::class);
