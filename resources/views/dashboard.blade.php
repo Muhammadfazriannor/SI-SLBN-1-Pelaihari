6a5dcfe5041e8a5dc8d2a3586bbc3c963ef43f2a
ricky-qwerty <ricky2633oke@gmail.com>
2024-11-12 14:37:25 +0800

    push

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -56,7 +56,7 @@
                             <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav">
                                     <a class="nav-link" href="pengumumen">Tambah Berita</a>
-                                    <a class="nav-link" href="layout-sidenav-light.html">Tambah PPDB</a>
+                                    <a class="nav-link" href="pendaftars">Tambah Pendaftar</a>
                                 </nav>
                             </div>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
