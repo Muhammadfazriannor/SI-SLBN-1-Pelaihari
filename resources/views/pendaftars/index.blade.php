08967015b7d01adedb10c1d6c1b96c45f8dc6cd0
ricky-qwerty <ricky2633oke@gmail.com>
2024-11-18 16:42:59 +0800

    push

--- resources/views/pendaftars/index.blade.php
+++ resources/views/pendaftars/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-<<<<<<< HEAD
+
     <h1 class="my-4 text-center">Pendaftar PPDB</h1>
     <div class="d-flex mb-3">
         <!-- Tombol untuk menambah pendaftar -->
@@ -91,49 +91,6 @@
         </div>
     </div>
     
-    <h1>Pendaftar PPDB</h1>
-    <a href="{{ route('pendaftars.create') }}" class="btn btn-primary">Tambah Pendaftar</a>
-    
-    @if(session('success'))
-        <div class="alert alert-success">{{ session('success') }}</div>
-    @endif
-
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Nama Lengkap</th>
-                <th>Tanggal Lahir</th>
-                <th>Jenis Kelamin</th>
-                <th>Alamat</th>
-                <th>Email</th>
-                <th>No HP</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($pendaftars as $pendaftar)
-            <tr>
-                <td>{{ $pendaftar->nama_lengkap }}</td>
-                <td>{{ $pendaftar->tanggal_lahir }}</td>
-                <td>{{ $pendaftar->jenis_kelamin }}</td>
-                <td>{{ $pendaftar->alamat }}</td>
-                <td>{{ $pendaftar->email }}</td>
-                <td>{{ $pendaftar->no_hp }}</td>
-                <td>
-                    <a href="{{ route('pendaftars.edit', $pendaftar) }}" class="btn btn-warning">Edit</a>
-                    <form action="{{ route('pendaftars.destroy', $pendaftar) }}" method="POST" style="display:inline;">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">Hapus</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-
-    {{ $pendaftars->links() }}
->>>>>>> a628b5d078c51e2cf77b451859dee30452e4f384
 </div>
 
 @endsection
