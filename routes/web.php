844a9936b0598a1ce8e313959c3ff68975f86b46
ricky-qwerty <ricky2633oke@gmail.com>
2024-11-19 13:49:36 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,13 @@ Route::get('/', function () {
 Route::resource('/pengumumen', PengumumanController::class);
 Route::resource('/pendaftars', PendaftarController::class);
 Route::get('pendaftars/{pendaftar}', [PendaftarController::class, 'show'])->name('pendaftars.show');
+Route::get('/pendaftars', [PendaftarController::class, 'index'])->name('pendaftars.index');
+
 
 
 Route::get('/seleksi', [SeleksiController::class, 'index'])->name('seleksi.index');
 Route::get('/seleksi/proses', [SeleksiController::class, 'prosesSeleksi'])->name('seleksi.proses');
 Route::get('/seleksi/update-status/{seleksi}/{status}', [SeleksiController::class, 'updateStatus'])->name('seleksi.updateStatus');
+
+
+
