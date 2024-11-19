9816b6066857a6285c915686f1f8529864f00cce
ricky-qwerty <ricky2633oke@gmail.com>
2024-11-19 13:48:56 +0800

    update migrasi pendaftars

--- database/migrations/2024_11_12_061102_create_pendaftars_table.php
+++ database/migrations/2024_11_12_061102_create_pendaftars_table.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,10 +12,10 @@ class CreatePendaftarsTable extends Migration
             $table->string('nama_lengkap');
             $table->date('tanggal_lahir');
             $table->string('jenis_kelamin');
-            $table->string('alamat');
-            $table->string('email');
+            $table->text('alamat');
+            $table->string('email')->unique();
             $table->string('no_hp');
-            $table->string('foto')->nullable(); // Optional jika foto tidak diupload
+            $table->string('foto')->nullable(); // Bisa null jika tidak ada foto
             $table->timestamps();
         });
     }
