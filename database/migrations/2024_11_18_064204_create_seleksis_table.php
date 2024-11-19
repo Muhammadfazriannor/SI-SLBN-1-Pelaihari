6b2aaa003c8dbab2dbc8fb7d352bef1d215e60f8
ricky-qwerty <ricky2633oke@gmail.com>
2024-11-19 13:49:13 +0800

    update migrasi seleksis

--- database/migrations/2024_11_18_064204_create_seleksis_table.php
+++ database/migrations/2024_11_18_064204_create_seleksis_table.php
@@ -1,29 +1,21 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateSeleksisTable extends Migration
 {
-    public function up()
+    public function up(): void
     {
         Schema::create('seleksis', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_lengkap');
-            $table->string('jenis_kelamin');
-            $table->date('tanggal_lahir');
-            $table->string('alamat');
-            $table->string('email');
-            $table->string('no_hp');
-            $table->string('foto')->nullable(); // Kolom foto
-            $table->boolean('lolos')->default(false); // Status seleksi
-            $table->string('status')->default('lihat'); // Kolom status seleksi
+            $table->foreignId('pendaftar_id')->constrained('pendaftars')->onDelete('cascade'); // Relasi ke pendaftar
+            $table->string('status')->default('lihat'); // Default status
             $table->timestamps();
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('seleksis');
     }
